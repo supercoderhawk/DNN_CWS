8b3cc60f944acac0e90ab1bc141e6c09e2f314b1
SuperCoderX <xyb930826@gmail.com>
2017-03-30 15:03:14 +0800

    optimize the code

--- cws.py
+++ cws.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import math
 import numpy as np
 import sys
+from datetime import datetime
 from prepare_data import read_train_data
 
 
@@ -46,7 +47,7 @@ def read_data(word_file_name, label_file_name, skip_window):
     label_batch.append(list(map(int, label.split(' '))))
   word_file.close()
   label_file.close()
-  return words_batch, label_batch
+  return np.array(words_batch), np.array(label_batch)
 
 
 def write_data(vocab_size, skip_window, word_file_name, label_file_name):
@@ -94,9 +95,14 @@ def train(vocab_size, embed_size, skip_window, word_file_name='word.txt',
     word_scores = tf.split(word_score, len(tags))
     # init_A = [[0.5,0.5,0,0],[1,0,0,0.15],[0,0,0.1,0],[1,0.01,0,0]]
     A = tf.Variable(
-      [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1], [1, 1, 0, 0]])
+      [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1], [1, 1, 0, 0]], dtype=tf.float32)
 
     param_list = [w2, w3, b2, b3]
+    param_grad = np.zeros([4,5]).tolist()
+    for w_index,w in enumerate(word_scores):
+      for p_index,p in enumerate(param_list):
+        param_grad[w_index][p_index] = tf.gradients(w,p)
+      param_grad[w_index][len(param_list)] = tf.gradients(w,x)
     # grad_list = [grad_w2, grad_w3, grad_b2, grad_b3]
 
     with tf.Session(graph=graph) as sess:
@@ -104,30 +110,43 @@ def train(vocab_size, embed_size, skip_window, word_file_name='word.txt',
       init.run()
       # 对每局句子进行参数更新
       for sentence_index, sentence in enumerate(words_batch):
+        start = datetime.now().timestamp()
         print('s:' + str(sentence_index))
+
         sentence_embeds = tf.reshape(tf.nn.embedding_lookup(embeddings, sentence),
                                      [len(sentence), window_length * embed_size, 1]).eval()
+        # print(sentence_embeds.shape)
+        sentence_scores = np.array(sess.run(word_score, feed_dict={x: sentence_embeds[0]}).T, dtype=np.float32)
 
-        sentence_scores = list()
-        for embed in sentence_embeds:
-          sentence_scores.append(tf.transpose(sess.run(word_score, feed_dict={x: embed})).eval())
+        for embed in sentence_embeds[1:, :]:
+          sentence_scores = np.append(sentence_scores, sess.run(word_score, feed_dict={x: embed}).T, 0)
 
-        current_tags = viterbi(np.reshape(sentence_scores, [len(sentence), len(tags), 1]), A.eval())
-
-        diff_tags = np.subtract(np.array(tags_batch[sentence_index]), np.array(current_tags)).tolist()
+        init_A_val = np.array(A.eval()[0])
+        A_val = np.array(A.eval()[1:])
+        current_tags = viterbi(sentence_scores, A_val, init_A_val)
+        # print(current_tags)
+        # print(current_tags.shape)
+        diff_tags = np.subtract(tags_batch[sentence_index], current_tags)
 
         for diff_index, diff_val in enumerate(diff_tags):
           if diff_val != 0:
-            pos_grad = word_scores[tags_batch[sentence_index][diff_index]]
-            neg_grad = word_scores[current_tags[diff_index]]
-            for param in param_list:
-              update_param(param, tf.gradients(pos_grad, param), x, sentence_embeds[diff_index], alpha, 1, sess)
-              update_param(param, tf.gradients(neg_grad, param), x, sentence_embeds[diff_index], alpha, -1, sess)
-
-            grad_x_pos_val = sess.run(tf.gradients(pos_grad, x),feed_dict={x: sentence_embeds[diff_index]})
-            grad_x_neg_val = sess.run(tf.gradients(neg_grad, x),feed_dict={x: sentence_embeds[diff_index]})
-            update_embeddings(embeddings, sentence[diff_index], alpha, 1,grad_x_pos_val[0], embed_size)
-            update_embeddings(embeddings, sentence[diff_index], alpha, -1,grad_x_neg_val[0], embed_size)
+            pos_grad_index = tags_batch[sentence_index][diff_index]
+            neg_grad_index = current_tags[diff_index]
+            for param_index,param in enumerate(param_list):
+              # print(sentence_embeds[diff_index,:].shape)
+              #update_param(param, tf.gradients(pos_grad, param), x, sentence_embeds[diff_index], alpha, 1, sess)
+              update_param(param, param_grad[pos_grad_index][param_index], x, sentence_embeds[diff_index], alpha, 1, sess)
+              #update_param(param, tf.gradients(neg_grad, param), x, sentence_embeds[diff_index], alpha, -1, sess)
+              update_param(param, param_grad[neg_grad_index][param_index], x, sentence_embeds[diff_index], alpha, -1, sess)
+            start = datetime.now().timestamp()
+            #tf.gradients(pos_grad, x)
+            #print(datetime.now().timestamp() - start)
+            #grad_x_pos_val = sess.run(tf.gradients(pos_grad, x), feed_dict={x: sentence_embeds[diff_index]})
+            grad_x_pos_val = sess.run(param_grad[pos_grad_index][len(param_list)], feed_dict={x: sentence_embeds[diff_index]})
+            #grad_x_neg_val = sess.run(tf.gradients(neg_grad, x), feed_dict={x: sentence_embeds[diff_index]})
+            grad_x_neg_val = sess.run(param_grad[neg_grad_index][len(param_list)], feed_dict={x: sentence_embeds[diff_index]})
+            update_embeddings(embeddings, sentence[diff_index], alpha, 1, grad_x_pos_val[0], embed_size)
+            update_embeddings(embeddings, sentence[diff_index], alpha, -1, grad_x_neg_val[0], embed_size)
 
             if diff_index == 0:
               tf.scatter_nd_add(A, [[0, tags_batch[sentence_index][diff_index]]], [alpha])
@@ -136,25 +155,28 @@ def train(vocab_size, embed_size, skip_window, word_file_name='word.txt',
               before = tags_batch[sentence_index][diff_index - 1]
               tf.scatter_nd_add(A, [[before, tags_batch[sentence_index][diff_index]]], [alpha])
               tf.scatter_nd_add(A, [[current_tags[diff_index - 1], current_tags[diff_index]]], [-alpha])
+        print(datetime.now().timestamp() - start)
 
 
 def update_embeddings(embeddings, indices, alpha, delta_grad, val, embed_size):
-  embeddings_val = embeddings.eval()
-  offset = -1 * embed_size
-  for index in indices:
-    offset += embed_size
-    for i, v in enumerate(embeddings_val[index]):
-      embeddings_val[index][i] += alpha * delta_grad * val[offset + i]
-  tf.assign(embeddings, embeddings_val)
+  #start = datetime.now().timestamp()
+  tf.scatter_nd_add(embeddings, np.expand_dims(indices, 1), (alpha * delta_grad * val).reshape(3, embed_size))
+  #print(datetime.now().timestamp()-start)
 
 
 def update_param(param, grad, x, x_val, alpha, delta_grad, sess):
+  #start = datetime.now().timestamp()
   grad_val = sess.run(grad, feed_dict={x: x_val})
+  #print(datetime.now().timestamp() - start)
+  #start = datetime.now().timestamp()
+  tf.assign_add(param, alpha * delta_grad * grad_val[0])
+  #print(datetime.now().timestamp() - start)
 
-  tf.assign(param,np.add(param.eval(), alpha * delta_grad * np.array(grad_val)[0]))
+
+TAG_MAPS = np.array([[0, 1], [2, 3], [2, 3], [0, 1]], dtype=np.int32)
 
 
-def viterbi(emission, A):
+def viterbi(emission, A, init_A):
   """
   维特比算法的实现，
   :param emission: 发射概率矩阵，对应于本模型中的分数矩阵
@@ -162,28 +184,19 @@ def viterbi(emission, A):
   :return:
   """
 
-  tags = [1, 2, 3, 4]
-  path = {0: [1], 1: [2]}
-  path_score = {0: [A[0][0] + emission[0][0]], 1: [A[0][1] + emission[0][1]]}
-  tag_maps = {1: [1, 2], 2: [3, 4], 3: [3, 4], 4: [1, 2]}
+  path = np.array([[0], [1]], dtype=np.int32)
+  print(emission.shape)
+  path_score = np.array([[init_A[0] + emission[0, 0]], [init_A[1] + emission[0, 1]]], dtype=np.float32)
 
-  for line_index, line in enumerate(emission):
-    if (line_index == 0):
-      continue
+  for line_index in range(1, emission.shape[0]):
+    last_index = path[:, -1]
+    cur_index = TAG_MAPS[last_index]  # 当前所有路径的可选的标记矩阵，2x2
+    cur_res = A[last_index, cur_index] + emission[line_index, cur_index] + np.expand_dims(path_score[:, -1], 1)
+    cur_max_index = np.argmax(cur_res, 1)
+    path = np.insert(path, [path.shape[1]], np.expand_dims(np.choose(cur_max_index, cur_index.T), 1), 1)
+    path_score = np.insert(path_score, [path_score.shape[1]], np.expand_dims(np.choose(cur_max_index, cur_res.T), 1), 1)
 
-    for path_index, _ in enumerate(path):
-      last_index = path[path_index][line_index - 1]
-      tag_map = tag_maps[last_index]
-      (score, tag) = max(
-        [(path_score[path_index][line_index - 1] + emission[line_index][i - 1] + A[last_index][i - 1], i)
-         for i in tag_map])
-      path[path_index].append(tag)
-      path_score[path_index].append(score)
-
-  if path_score[0][len(emission) - 1] < path[1][len(emission) - 1]:
-    return list(map(lambda x: x - 1, path[1]))
-  else:
-    return list(map(lambda x: x - 1, path[0]))
+  return path[np.argmax(path_score[:, -1]), :]
 
 
 if __name__ == '__main__':
