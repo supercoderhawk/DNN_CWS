f92d16d164e9cecb098f1b7290c9ade624956ffa
SuperCoderX <xyb930826@gmail.com>
2017-04-11 09:27:04 +0800

    remove Reduced

--- SegMMTNN.py
+++ SegMMTNN.py
@@ -1,8 +1,14 @@
 #-*- coding: UTF-8 -*-
+import tensorflow as tf
+import numpy as np
 
 class SegMMTNN:
   def __init__(self):
     pass
+
+  def train(self):
+    pass
+    #self.w1 =
 
 if __name__ == '__main__':
   pass
