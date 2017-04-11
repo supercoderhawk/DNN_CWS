06545ab0f24ce46fcc50aa48caae77a98a8b8e30
SuperCoderX <xyb930826@gmail.com>
2017-04-11 19:46:49 +0800

    add seg base class

---
+++ seg_base.py
@@ -0,0 +1,31 @@
+#-*- coding: UTF-8 -*-
+import numpy as np
+
+
+class SegBase:
+  def __init__(self):
+    self.TAG_MAPS = np.array([[0, 1], [2, 3], [2, 3], [0, 1]], dtype=np.int32)
+
+  def viterbi(self, emission, A, init_A):
+    """
+    维特比算法的实现，
+    :param emission: 发射概率矩阵，对应于本模型中的分数矩阵
+    :param A: 转移概率矩阵
+    :return:
+    """
+
+    path = np.array([[0], [1]], dtype=np.int32)
+    print(emission.shape)
+    path_score = np.array([[init_A[0] + emission[0, 0]], [init_A[1] + emission[0, 1]]], dtype=np.float32)
+
+    for line_index in range(1, emission.shape[0]):
+      last_index = path[:, -1]
+      cur_index = self.TAG_MAPS[last_index]  # 当前所有路径的可选的标记矩阵，2x2
+      # 当前所有可能路径的分值
+      cur_res = A[last_index, cur_index] + emission[line_index, cur_index] + np.expand_dims(path_score[:, -1], 1)
+      cur_max_index = np.argmax(cur_res, 1)
+      path = np.insert(path, [path.shape[1]], np.expand_dims(np.choose(cur_max_index, cur_index.T), 1), 1)
+      path_score = np.insert(path_score, [path_score.shape[1]], np.expand_dims(np.choose(cur_max_index, cur_res.T), 1),
+                             1)
+
+    return path[np.argmax(path_score[:, -1]), :]
