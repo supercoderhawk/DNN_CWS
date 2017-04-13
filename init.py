8d2b58762de2ceef68bf887cc039e52b2a0ba0bb
SuperCoderX <xyb930826@gmail.com>
2017-04-13 10:19:44 +0800

    add copy dict

--- init.py
+++ init.py
@@ -2,11 +2,14 @@
 from prepare_data import PrepareData
 from transform_data_dnn import TransformDataDNN
 import constant
+from shutil import copyfile
 
 def init():
   prepare_pku = PrepareData(constant.VOCAB_SIZE, 'corpus/pku_training.utf8', 'corpus/pku_training_words.txt',
                             'corpus/pku_training_labels.txt', 'corpus/pku_training_dict.txt')
   prepare_pku.build_exec()
+  dict_name = 'corpus/pku_training_dict.txt'
+  copyfile(dict_name,'corpus/dict.txt')
   trans_dnn = TransformDataDNN(constant.DNN_SKIP_WINDOW, True)
   trans_dnn.generate_exe()
 
