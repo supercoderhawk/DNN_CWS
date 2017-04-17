7cbd55db22791ff0d465200ce295172ad55454f2
SuperCoderX <xyb930826@gmail.com>
2017-04-17 17:06:00 +0800

    change filename

--- init.py
+++ init.py
@@ -9,7 +9,7 @@ def init():
                             'corpus/pku_training_labels.txt', 'corpus/pku_training_dict.txt')
   prepare_pku.build_exec()
   dict_name = 'corpus/pku_training_dict.txt'
-  copyfile(dict_name,'corpus/dict.txt')
+  copyfile(dict_name,'corpus/dict.utf8')
   trans_dnn = TransformDataDNN(constant.DNN_SKIP_WINDOW, True)
   trans_dnn.generate_exe()
 
