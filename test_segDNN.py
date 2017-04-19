87a7edfed00d281c38e8b83136aa581ecaf84d01
SuperCoderX <xyb930826@gmail.com>
2017-04-19 10:58:51 +0800

    add test code

--- test_segDNN.py
+++ test_segDNN.py
@@ -12,18 +12,20 @@ class TestSegDNN(TestCase):
 
   def test_gen_update_A(self):
     a = np.array([0, 0, 0, 1, 3], dtype=np.int32)
-    b = np.array([1, 1, 3, 1, 3], dtype=np.int32)
-    A_correct = np.array([[2,0,0,0],[0,0,0,-1],[0,0,0,0],[0,-1,0,0]],dtype=np.int32)
+    b = np.array([1, 3, 0, 1, 3], dtype=np.int32)
+    A_correct = np.array([[2,0,0,0],[0,0,0,-1],[0,0,0,0],[-1,0,0,0]],dtype=np.int32)
     init_A_correct = np.array([1,-1,0,0])
     A_currect,init_A_currect,init_update = self.cws.gen_update_A(a, b)
     self.assertTrue(np.all(A_correct==A_currect))
     self.assertTrue(np.all(init_A_correct == init_A_currect))
 
   def test_viterbi(self):
-    initial_prob = np.array([[0.6], [0.4]])
-    trans_prob = np.array([[0.7, 0.3], [0.4, 0.6]])
-    obs_prob = np.array([[0.5, 0.4, 0.1], [0.1, 0.3, 0.6]])
-    observations = np.array([0, 1, 1, 2, 1])
+    score = np.arange(10,170,10).reshape(4,4).T
+    A = np.array([[1,1,0,0],[0,0,1,1],[0,0,1,1],[1,1,0,0]])
+    init_A = np.array([1,1,0,0])
+    path = self.cws.viterbi(score,A,init_A)
+    correct_path = np.array([1,3,1,3])
+    self.assertTrue(np.all(path == correct_path))
 
   def test_sentence2index(self):
     pass
