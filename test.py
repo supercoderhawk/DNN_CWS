750f31b45d5c86d912d890cb29dbd0d939e2d51d
SuperCoderX <xyb930826@gmail.com>
2017-04-20 11:09:22 +0800

    add test cws file

---
+++ test.py
@@ -0,0 +1,14 @@
+#-*- coding: UTF-8 -*-
+
+from seg_dnn import SegDNN
+import constant
+
+def test_seg_dnn():
+  cws = SegDNN(constant.VOCAB_SIZE,50,constant.DNN_SKIP_WINDOW)
+  model = 'tmp/model0.ckpt'
+  print(cws.seg('小明来自南京师范大学',model))
+  print(cws.seg('小明是上海理工大学的学生',model))
+  print(cws.seg('迈向充满希望的新世纪',model))
+
+if __name__ == '__main__':
+  test_seg_dnn()
