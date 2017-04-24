76637b2b2ece2571b2cdf733b7d8dd8f6eeaf60e
SuperCoderX <xyb930826@gmail.com>
2017-04-24 19:00:04 +0800

    change test file

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import constant
 
 def test_seg_dnn():
   cws = SegDNN(constant.VOCAB_SIZE,50,constant.DNN_SKIP_WINDOW)
-  model = 'tmp/model0.ckpt'
+  model = 'tmp/model3.ckpt'
   print(cws.seg('小明来自南京师范大学',model))
   print(cws.seg('小明是上海理工大学的学生',model))
   print(cws.seg('迈向充满希望的新世纪',model))
