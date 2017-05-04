631bd9e0580954b87cf09b8445ce0d4ef76157ad
SuperCoderX <xyb930826@gmail.com>
2017-05-04 10:55:01 +0800

    do some changes to seg_dnn

--- seg_dnn.py
+++ seg_dnn.py
@@ -25,29 +25,28 @@ class SegDNN:
     self.words_batch = trans_dnn.words_batch
     self.tags_batch = trans_dnn.labels_batch
     self.words_count = trans_dnn.words_count
-    self.context_count = trans_dnn.context_count
     self.sess = None
-    self.x = tf.placeholder(tf.float32, shape=[self.concat_embed_size, None], name='x')
-    self.map_matrix = tf.placeholder(tf.float32, shape=[4, None], name='mm')
+    self.x = tf.placeholder(tf.float64, shape=[self.concat_embed_size, None], name='x')
+    self.map_matrix = tf.placeholder(tf.float64, shape=[4, None], name='mm')
     self.embeddings = tf.Variable(
-      tf.random_uniform([self.vocab_size, self.embed_size], -1.0 / self.embed_size, 1.0 / self.embed_size,
-                        dtype=tf.float32), dtype=tf.float32, name='embeddings')
-    # self.embeddings = tf.Variable(np.load('data/exp_embeddings.npy'), dtype=tf.float32, name='embeddings')
+      tf.random_uniform([self.vocab_size, self.embed_size], -1.0 / math.sqrt(self.embed_size), 1.0 / math.sqrt(self.embed_size),
+                        dtype=tf.float64), dtype=tf.float64, name='embeddings')
     self.w2 = tf.Variable(
-      tf.truncated_normal([self.h, self.concat_embed_size], stddev=1.0 / math.sqrt(self.concat_embed_size),
-                          dtype=tf.float32), dtype=tf.float32, name='w2')
-    self.b2 = tf.Variable(tf.zeros([self.h, 1], dtype=tf.float32), dtype=tf.float32, name='b2')
+        tf.random_uniform([self.h, self.concat_embed_size], -4.0/math.sqrt(self.concat_embed_size),4/math.sqrt(self.concat_embed_size),
+                            dtype=tf.float64), dtype=tf.float64, name='w2')
+    self.b2 = tf.Variable(tf.zeros([self.h, 1], dtype=tf.float64), dtype=tf.float64, name='b2')
+
     self.w3 = tf.Variable(
-      tf.random_uniform([self.tags_count, self.h], -1.0 / math.sqrt(self.h), 1.0 / math.sqrt(self.h), dtype=tf.float32),
-      dtype=tf.float32, name='w3')
-    self.b3 = tf.Variable(tf.zeros([self.tags_count, 1], dtype=tf.float32), dtype=tf.float32, name='b3')
+      tf.random_uniform([self.tags_count, self.h], -4.0/math.sqrt(self.h), 4.0 / math.sqrt(self.h), dtype=tf.float64),
+      dtype=tf.float64, name='w3')
+    self.b3 = tf.Variable(tf.zeros([self.tags_count, 1], dtype=tf.float64), dtype=tf.float64, name='b3')
     self.word_score = tf.add(tf.matmul(self.w3, tf.sigmoid(tf.add(tf.matmul(self.w2, self.x), self.b2))), self.b3)
     self.params = [self.w2, self.b2, self.w3, self.b3]
-    self.A = tf.Variable([[1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1], [1, 1, 0, 0]], dtype=tf.float32, name='A')
-    self.init_A = tf.Variable([1, 1, 0, 0], dtype=tf.float32, name='init_A')
-    self.Ap = tf.placeholder(tf.float32, shape=self.A.get_shape())
-    self.init_Ap = tf.placeholder(tf.float32, shape=self.init_A.get_shape())
-    self.embedp = tf.placeholder(tf.float32, shape=[None, self.embed_size])
+    self.A = tf.Variable(tf.random_uniform([4, 4], -1, 1,dtype=tf.float64), dtype=tf.float64, name='A')
+    self.init_A = tf.Variable(tf.random_uniform([4], -1, 1,dtype=tf.float64), dtype=tf.float64, name='init_A')
+    self.Ap = tf.placeholder(tf.float64, shape=self.A.get_shape())
+    self.init_Ap = tf.placeholder(tf.float64, shape=self.init_A.get_shape())
+    self.embedp = tf.placeholder(tf.float64, shape=[None, self.embed_size])
     self.embed_index = tf.placeholder(tf.int32, shape=[None])
     self.update_embed_op = tf.scatter_update(self.embeddings, self.embed_index, self.embedp)
     self.lam = 0.0001
@@ -58,23 +57,19 @@ class SegDNN:
     self.mul_init_A_op = tf.assign(self.init_A, tf.scalar_mul(1 - self.lam, self.init_A))
     self.loss = -tf.reduce_sum(tf.multiply(self.map_matrix, self.word_score))
     self.loss_plus = [0] * 4
-
     for i in range(4):
       self.loss_plus[i] = tf.assign_sub(self.params[i], tf.scalar_mul(self.lam, self.params[i]))
-      # tf.assign_sub(self.params[i],tf.multiply(self.params[i]))
-      # self.loss_plus[i] = tf.multiply(self.params[i], 1 - self.lam)
     self.grad_embed = tf.gradients(tf.multiply(self.map_matrix, self.word_score), self.x)
     self.update_embed = self.alpha * (self.grad_embed[0]) + (1 - self.lam) * self.x
     self.train_loss = self.optimizer.minimize(self.loss, var_list=self.params)
     self.indices = tf.placeholder(tf.int32, shape=[None, 2])
     self.shape = tf.placeholder(tf.int32, shape=[2])
-    self.values = tf.placeholder(tf.float32, shape=[None])
-    self.pos_indices = tf.placeholder(tf.int32, shape=[None, 2])
-    self.neg_indices = tf.placeholder(tf.int32, shape=[None, 2])
+    self.values = tf.placeholder(tf.float64, shape=[None])
     self.gen_map = tf.sparse_to_dense(self.indices, self.shape, self.values, validate_indices=False)
     self.sentence_holder = tf.placeholder(tf.int32, shape=[None, self.window_length])
     self.lookup_op = tf.nn.embedding_lookup(self.embeddings, self.sentence_holder)
     self.line_index = np.arange(4, dtype=np.int32)
+    self.sentence_index = 0
 
   def train(self):
     """
@@ -94,10 +89,15 @@ class SegDNN:
     init.run(session=self.sess)
     self.sess.graph.finalize()
     loss = []
-    count = 16
+    count = 10
+
+    w2 = self.w2.eval(session=self.sess)
+    with open('tmp/w2.txt','w') as w2f:
+      w2s = '\n'.join(list(map(lambda l: ' '.join(list(map(lambda d:str(d),l))),w2)))
+      w2f.write(w2s)
 
     for i in range(count):
-      loss.append(self.train_exe() / 100000000)
+      loss.append(self.train_exe() / 10000)
       print(i)
       saver.save(self.sess, 'tmp/model%d.ckpt' % i)
       train_writer.flush()
@@ -109,8 +109,9 @@ class SegDNN:
     start = time.time()
     time_all = 0.0
     start_c = 0
-
+    print(self.w3.eval(session=self.sess))
     for sentence_index, (sentence, tags) in enumerate(zip(self.words_batch, self.tags_batch)):
+      self.sentence_index = sentence_index
       start_s = time.time()
       self.train_sentence(sentence, tags, len(tags))
       start_c += time.time() - start_s
@@ -136,7 +137,7 @@ class SegDNN:
 
     init_A_val = self.init_A.eval(session=self.sess)
     A_val = self.A.eval(session=self.sess)
-    current_tags = self.viterbi_fd(sentence_scores, A_val, init_A_val)  # 当前参数下的最优路径
+    current_tags = self.viterbi(sentence_scores, A_val, init_A_val)  # 当前参数下的最优路径
     diff_tags = np.subtract(tags, current_tags)
     update_index = np.where(diff_tags != 0)[0]  # 标签不同的字符位置
     update_length = len(update_index)
@@ -155,7 +156,6 @@ class SegDNN:
     output_shape = [4, update_length]
     sentence_matrix = self.sess.run(self.gen_map, feed_dict={self.indices: sparse_indices, self.shape: output_shape,
                                                              self.values: sparse_values})
-
     self.update_params(sentence_matrix, update_embed, sentence[update_index], update_length)
     # 更新转移矩阵
     A_update, init_A_update, update_init = self.gen_update_A(tags, current_tags)
@@ -181,8 +181,8 @@ class SegDNN:
 
     for i in range(update_length):
       embed = np.expand_dims(embeds[:, i], 1)
-      grad = self.sess.run(self.update_embed, feed_dict={self.x: embed,
-                                                         self.map_matrix: np.expand_dims(sen_matrix[:, i], 1)})[0]
+      grad = self.sess.run(self.grad_embed, feed_dict={self.x: embed,
+                                                       self.map_matrix: np.expand_dims(sen_matrix[:, i], 1)})[0]
       update_embed = (embed + self.alpha * grad) * (1 - self.lam)
       self.embeddings = self.sess.run(self.update_embed_op,
                                       feed_dict={
@@ -190,8 +190,8 @@ class SegDNN:
                                         self.embed_index: embed_index[i, :]})
 
   def gen_update_A(self, correct_tags, current_tags):
-    A_update = np.zeros([4, 4], dtype=np.float32)
-    init_A_update = np.zeros([4], dtype=np.float32)
+    A_update = np.zeros([4, 4], dtype=np.float64)
+    init_A_update = np.zeros([4], dtype=np.float64)
     before_corr = correct_tags[0]
     before_curr = current_tags[0]
     update_init = False
@@ -210,7 +210,7 @@ class SegDNN:
 
     return A_update, init_A_update, update_init
 
-  def viterbi_fd(self, emission, A, init_A, return_score=False):
+  def viterbi(self, emission, A, init_A, return_score=False):
     """
     维特比算法的实现，所有输入和返回参数均为numpy数组对象
     :param emission: 发射概率矩阵，对应于本模型中的分数矩阵，4*length
@@ -223,14 +223,14 @@ class SegDNN:
     length = emission.shape[1]
     path = np.ones([4, length], dtype=np.int32) * -1
     corr_path = np.zeros([length], dtype=np.int32)
-    path_score = np.ones([4, length], dtype=np.float32) * np.finfo('f').min
+    path_score = np.ones([4, length], dtype=np.float64) * (np.finfo('f').min / 2)
     path_score[:, 0] = init_A + emission[:, 0]
 
     for pos in range(1, length):
       for t in range(4):
         for prev in range(4):
           temp = path_score[prev][pos - 1] + A[prev][t] + emission[t][pos]
-          if temp > path_score[t][pos]:
+          if temp >= path_score[t][pos]:
             path[t][pos] = prev
             path_score[t][pos] = temp
 
@@ -240,10 +240,8 @@ class SegDNN:
       max_index = path[max_index][i]
       corr_path[i - 1] = max_index
     if return_score:
-      # return np.insert(path[max_index, 1:],[length-1],max_index), path_score[max_index, :]
       return corr_path, path_score[max_index, :]
     else:
-      # return np.insert(path[max_index, 1:],[length-1],max_index)
       return corr_path
 
   def cal_sentence_loss(self, sentence, tags, length):
@@ -253,7 +251,7 @@ class SegDNN:
 
     init_A_val = self.init_A.eval(session=self.sess)
     A_val = self.A.eval(session=self.sess)
-    current_tags = self.viterbi_fd(sentence_scores, A_val, init_A_val)  # 当前参数下的最优路径
+    current_tags = self.viterbi(sentence_scores, A_val, init_A_val)  # 当前参数下的最优路径
     loss = 0.0
     before_corr = 0
     before_cur = 0
@@ -287,7 +285,6 @@ class SegDNN:
     return seq
 
   def tags2words(self, sentence, tags):
-    print(tags)
     words = []
     word = ''
     for tag_index, tag in enumerate(tags):
@@ -306,45 +303,47 @@ class SegDNN:
 
     return words
 
-  def seg(self, sentence, model_path='tmp/model0.ckpt'):
-    dtype = tf.float32
+  def seg(self, sentence, model_path='model/model.ckpt'):
+    dtype = tf.float64
     tf.reset_default_graph()
     x = tf.placeholder(dtype, shape=[self.concat_embed_size, None], name='x')
-    embeddings = tf.Variable(tf.random_uniform([self.vocab_size, self.embed_size], -1.0, 1.0, dtype=tf.float32),
-                             tf.float32, name='embeddings')
-    # embeddings = tf.Variable(np.load('data/dnn/embeddings.npy'), dtype, name='embeddings')
-    w2 = tf.Variable(
-      tf.truncated_normal([self.h, self.concat_embed_size], stddev=1.0 / math.sqrt(self.concat_embed_size),
-                          dtype=tf.float32), dtype=tf.float32, name='w2')
-    # w2 = tf.Variable(np.load('data/dnn/w2.npy'), dtype, name='w2')
-    b2 = tf.Variable(tf.zeros([self.h, 1], dtype=tf.float32), dtype=tf.float32, name='b2')
-    # b2 = tf.Variable(np.load('data/dnn/b2.npy'), dtype, name='b2')
-    w3 = tf.Variable(
-      tf.truncated_normal([self.tags_count, self.h], stddev=1.0 / math.sqrt(self.concat_embed_size), dtype=tf.float32),
-      dtype=tf.float32, name='w3')
-    # w3 = tf.Variable(np.load('data/dnn/w3.npy'), dtype, name='w3')
-    b3 = tf.Variable(tf.zeros([self.tags_count, 1], dtype=tf.float32), dtype=tf.float32, name='b3')
-    # b3 = tf.Variable(np.load('data/dnn/b3.npy'), dtype, name='b3')
+    #embeddings = tf.Variable(tf.random_uniform([self.vocab_size, self.embed_size], -1.0, 1.0, dtype=tf.float64),
+    #                         tf.float64, name='embeddings')
+    embeddings = tf.Variable(np.load('data/dnn/embeddings.npy'), dtype, name='embeddings')
+    #w2 = tf.Variable(
+    #  tf.truncated_normal([self.h, self.concat_embed_size], stddev=1.0 / math.sqrt(self.concat_embed_size),
+    #                      dtype=tf.float64), dtype=tf.float64, name='w2')
+    w2 = tf.Variable(np.load('data/dnn/w2.npy'), dtype, name='w2')
+    #b2 = tf.Variable(tf.zeros([self.h, 1], dtype=tf.float64), dtype=tf.float64, name='b2')
+    b2 = tf.Variable(np.load('data/dnn/b2.npy'), dtype, name='b2')
+    #w3 = tf.Variable(
+    #  tf.truncated_normal([self.tags_count, self.h], stddev=1.0 / math.sqrt(self.concat_embed_size), dtype=tf.float64),
+    #  dtype=tf.float64, name='w3')
+    w3 = tf.Variable(np.load('data/dnn/w3.npy'), dtype, name='w3')
+    #b3 = tf.Variable(tf.zeros([self.tags_count, 1], dtype=tf.float64), dtype=tf.float64, name='b3')
+    b3 = tf.Variable(np.load('data/dnn/b3.npy'), dtype, name='b3')
     word_score = tf.matmul(w3, tf.sigmoid(tf.matmul(w2, x) + b2)) + b3
-    A = tf.Variable(
-      [[1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1], [1, 1, 0, 0]], dtype=tf.float32, name='A')
-    # A = tf.Variable(np.load('data/dnn/A.npy'), dtype, name='A')
-    init_A = tf.Variable([1, 1, 0, 0], dtype=tf.float32, name='init_A')
-    # init_A = tf.Variable(np.load('data/dnn/init_A.npy'), dtype, name='init_A')
+    #A = tf.Variable(
+    #  [[1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1], [1, 1, 0, 0]], dtype=tf.float64, name='A')
+    A = tf.Variable(np.load('data/dnn/A.npy'), dtype, name='A')
+    #init_A = tf.Variable([1, 1, 0, 0], dtype=tf.float64, name='init_A')
+    init_A = tf.Variable(np.load('data/dnn/init_A.npy'), dtype, name='init_A')
     saver = tf.train.Saver()
     with tf.Session() as sess:
       saver.restore(sess, model_path)
-      # tf.global_variables_initializer().run()
+      #tf.global_variables_initializer().run()
+      #saver.save(sess,'model/model.ckpt')
       seq = self.index2seq(self.sentence2index(sentence))
       sentence_embeds = tf.nn.embedding_lookup(embeddings, seq).eval().reshape(
         [len(sentence), self.concat_embed_size]).T
       sentence_scores = sess.run(word_score, feed_dict={x: sentence_embeds})
       init_A_val = init_A.eval()
       A_val = A.eval()
-      # print(A_val)
+      #print(A_val)
       # print(init_A_val)
-      current_tags = self.viterbi_fd(sentence_scores, A_val, init_A_val)
-      # print(sentence_scores.T)
+      current_tags = self.viterbi(sentence_scores, A_val, init_A_val)
+      #print(sentence_scores.T)
+      #print(sentence_embeds[:,2])
       w3v = w3.eval().T.tolist()
       # print(w3v)
       file = open('tmp/w3.txt', 'w')
@@ -354,8 +353,16 @@ class SegDNN:
         file.write(' '.join(v) + '\n')
       file.close()
 
-      return self.tags2words(sentence, current_tags)
+      w2v = w2.eval().T.tolist()
+      # print(w3v)
+      file = open('tmp/w22.txt', 'w')
 
+      for i, v in enumerate(w2v):
+        v = list(map(lambda f: str(f), v))
+        file.write(' '.join(v) + '\n')
+      file.close()
+
+      return self.tags2words(sentence, current_tags),current_tags
 
 if __name__ == '__main__':
   embed_size = 50
